041ac88b7b6bfa20b3e995e03da448a519e3dad3
italosanches <italo_carvalho157@hotmail.com>
2024-07-18 09:27:59 -0300

    Models

---
+++ APILivraria/Models/Autor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APILivraria.Models
+{
+	[Table("Autores")]
+	public class Autor
+	{
+        [Key]
+        public int AutorId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? AutorNome { get; set; }
+
+        public virtual ICollection<Livro> Livros { get; set; } = new List<Livro>();
+    }
+}
---
+++ APILivraria/Models/Livro.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APILivraria.Models
+{
+	[Table("Livros")]
+	public class Livro
+	{
+		[Key]
+        public int		 LivroId      { get; set; }
+
+		[Required]
+		[MaxLength(100)]
+        public string? LivroNome      { get; set; }
+
+		[MaxLength(500)]
+        public string? LivroDescricao { get; set; }
+		[Range(0,999)]
+		public int LivroQtdPaginas {  get; set; }
+
+		[ForeignKey(nameof(Autor.AutorId))]
+		public int AutorId            { get; set; }
+        public virtual Autor? Autor   { get; set; }
+    }
+}
