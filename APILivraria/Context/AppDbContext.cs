237b1bcbcd8db36c9617832fd8c18b2e13991e18
italosanches <italo_carvalho157@hotmail.com>
2024-07-18 10:03:40 -0300

    Migrations

---
+++ APILivraria/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using APILivraria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APILivraria.Context
+{
+	public class AppDbContext : DbContext
+	{
+		DbSet<Autor> Autores { get; set; }
+		DbSet<Livro> Livros { get; set; }
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Autor>()
+				.HasMany(a => a.Livros)
+				.WithOne(b => b.Autor)
+				.HasForeignKey(b => b.AutorId);
+
+	
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ APILivraria/Migrations/20240718130115_MigrationInitial.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APILivraria.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    AutorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AutorNome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.AutorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    LivroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LivroNome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LivroDescricao = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    LivroQtdPaginas = table.Column<int>(type: "int", nullable: false),
+                    AutorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.LivroId);
+                    table.ForeignKey(
+                        name: "FK_Livros_Autores_AutorId",
+                        column: x => x.AutorId,
+                        principalTable: "Autores",
+                        principalColumn: "AutorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_AutorId",
+                table: "Livros",
+                column: "AutorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Livros");
+
+            migrationBuilder.DropTable(
+                name: "Autores");
+        }
+    }
+}
