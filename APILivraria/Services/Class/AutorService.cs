ecd7ffccb0384c60a11589d19642b48fc5f0769d
italosanches <italo_carvalho157@hotmail.com>
2024-07-23 14:40:55 -0300

    Criacao de service Autor

---
+++ APILivraria/Services/Class/AutorService.cs
@@ -0,0 +1,41 @@
+﻿using APILivraria.Models;
+using APILivraria.Repository;
+using APILivraria.Services.Interfaces;
+
+namespace APILivraria.Services.Class
+{
+	public class AutorService : IAutorService
+	{
+		private readonly IAutorRepository _repository;
+
+		public AutorService(IAutorRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public Task<int> CreateAsync(Autor autor)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> DeleteAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Autor> GetAutorByIdAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<Autor>> GetAutoresAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> UpdateAsync(Autor autor, int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ APILivraria/Services/Interfaces/IAutorService.cs
@@ -0,0 +1,13 @@
+﻿using APILivraria.Models;
+
+namespace APILivraria.Services.Interfaces
+{
+    public interface IAutorService
+    {
+        Task<IEnumerable<Autor>> GetAutoresAsync();
+        Task<Autor> GetAutorByIdAsync(int id);
+        Task<int> CreateAsync(Autor autor);
+        Task<bool> UpdateAsync(Autor autor, int id);
+        Task<bool> DeleteAsync(int id);
+    }
+}
